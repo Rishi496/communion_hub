12770802fb1d936bab2f5a9e69b9d92d18c5d857
Rishi496 <rishirsh21@gmail.com>
2025-04-01 23:35:39 +0530

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { motion } from "framer-motion";
 import { Link } from "react-router-dom";
-import "../styles/animations.css";
+import "../Styles/animations.css";
 
 const Home = () => {
   return (
